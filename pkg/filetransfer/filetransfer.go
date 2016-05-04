014e7c62667f79d4e15e4a36d393f364d4879c7f
Cheah Chu Yeow <chuyeow@gmail.com>
2016-05-04 08:22:28 +0000

    Add DeleteAll to FileTransfer to delete all objects matching a prefix.

--- pkg/filetransfer/filetransfer.go
+++ pkg/filetransfer/filetransfer.go
@@ -6,4 +6,5 @@ type FileTransfer interface {
 	Upload(region, bucket, key string, body io.Reader, contentType, acl string) error
 	Download(region, bucket, key string, out io.WriterAt) error
 	Delete(region, bucket string, keys ...string) error
+	DeleteAll(region, bucket, prefix string) error
 }
--- pkg/filetransfer/s3.go
+++ pkg/filetransfer/s3.go
@@ -21,7 +21,7 @@ func NewS3(partSize int64, maxUploadParts int) *S3 {
 	}
 }
 
-func (s *S3) Upload(region, bucket, key string, body io.Reader, contentType, acl string) (err error) {
+func (s *S3) Upload(region, bucket, key string, body io.Reader, contentType, acl string) error {
 	sess := session.New(&aws.Config{Region: aws.String(region)})
 	uploader := s3manager.NewUploader(sess, func(u *s3manager.Uploader) {
 		if s.partSize != 0 {
@@ -40,17 +40,17 @@ func (s *S3) Upload(region, bucket, key string, body io.Reader, contentType, acl
 		acl = "private"
 	}
 
-	_, err = uploader.Upload(&s3manager.UploadInput{
+	_, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket:      aws.String(bucket),
 		Key:         aws.String(key),
 		Body:        body,
 		ACL:         aws.String(acl),
 		ContentType: aws.String(contentType),
 	})
-	return
+	return err
 }
 
-func (s *S3) Download(region, bucket, key string, out io.WriterAt) (err error) {
+func (s *S3) Download(region, bucket, key string, out io.WriterAt) error {
 	sess := session.New(&aws.Config{Region: aws.String(region)})
 	downloader := s3manager.NewDownloader(sess, func(d *s3manager.Downloader) {
 		if s.partSize != 0 {
@@ -58,14 +58,14 @@ func (s *S3) Download(region, bucket, key string, out io.WriterAt) (err error) {
 		}
 	})
 
-	_, err = downloader.Download(out, &s3.GetObjectInput{
+	_, err := downloader.Download(out, &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
-	return
+	return err
 }
 
-func (s *S3) Delete(region, bucket string, keys ...string) (err error) {
+func (s *S3) Delete(region, bucket string, keys ...string) error {
 	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
 
 	var objects []*s3.ObjectIdentifier
@@ -83,6 +83,52 @@ func (s *S3) Delete(region, bucket string, keys ...string) (err error) {
 		Delete: &s3.Delete{Objects: objects},
 	}
 
-	_, err = svc.DeleteObjects(params)
+	_, err := svc.DeleteObjects(params)
 	return err
+}
+
+func (s *S3) DeleteAll(region, bucket, prefix string) error {
+	svc := s3.New(session.New(&aws.Config{Region: aws.String(region)}))
+
+	listInput := &s3.ListObjectsInput{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(prefix),
+	}
+
+	// This is slightly complex because ListObjectsPages() accepts a function
+	// that gets called for every page returned. The function should return
+	// false to stop iterating.
+	var fnErr error
+	err := svc.ListObjectsPages(listInput, func(res *s3.ListObjectsOutput, lastPage bool) (shouldContinue bool) {
+
+		if len(res.Contents) == 0 {
+			return false // Stop iterating.
+		}
+
+		objIdentifiers := make([]*s3.ObjectIdentifier, 0, len(res.Contents))
+		for _, obj := range res.Contents {
+			objIdentifiers = append(objIdentifiers, &s3.ObjectIdentifier{
+				Key:       obj.Key,
+				VersionId: nil,
+			})
+		}
+
+		_, fnErr = svc.DeleteObjects(&s3.DeleteObjectsInput{
+			Bucket: aws.String(bucket),
+			Delete: &s3.Delete{Objects: objIdentifiers},
+		})
+		if fnErr != nil {
+			return false // Stop iterating.
+		}
+
+		return !lastPage
+	})
+	if err != nil {
+		return err
+	}
+	if fnErr != nil {
+		return fnErr
+	}
+
+	return nil
 }
