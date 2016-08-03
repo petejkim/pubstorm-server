9fb19ff5d8ec4b3a7ddd485eb20cdeb636d06216
Cheah Chu Yeow <chuyeow@gmail.com>
2016-08-03 10:21:08 +0000

    Add model to store (project) templates.

---
+++ apiserver/models/template/template.go
@@ -0,0 +1,31 @@
+package template
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Template struct {
+	gorm.Model
+
+	Name            string
+	Rank            int
+	DownloadURL     string `sql:"column:download_url"`
+	PreviewURL      string `sql:"column:preview_url"`
+	PreviewImageURL string `sql:"column:preview_image_url"`
+}
+
+func (t *Template) AsJSON() interface{} {
+	return struct {
+		Name            string `json:"name"`
+		Rank            int    `json:"rank"`
+		DownloadURL     string `json:"download_url"`
+		PreviewURL      string `json:"preview_url"`
+		PreviewImageURL string `json:"preview_image_url"`
+	}{
+		Name:            t.Name,
+		Rank:            t.Rank,
+		DownloadURL:     t.DownloadURL,
+		PreviewURL:      t.PreviewURL,
+		PreviewImageURL: t.PreviewImageURL,
+	}
+}
