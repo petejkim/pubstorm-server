60179c752f82e05ec27481600ef902bfa51a9067
Cheah Chu Yeow <chuyeow@gmail.com>
2016-08-03 10:21:08 +0000

    Add model to store (project) templates.

---
+++ testhelper/factories/template.go
@@ -0,0 +1,35 @@
+package factories
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	"github.com/nitrous-io/rise-server/apiserver/models/template"
+
+	. "github.com/onsi/gomega"
+)
+
+var templateN = 0
+
+func Template(db *gorm.DB, rank int, name ...string) *template.Template {
+	var tName string
+	if len(name) > 0 {
+		tName = name[0]
+	} else {
+		templateN++
+		tName = fmt.Sprintf("template%04d", projectN)
+	}
+
+	tmpl := &template.Template{
+		Name:            tName,
+		Rank:            rank,
+		DownloadURL:     fmt.Sprintf("https://example.com/download/template-%s.tar.gz", tName),
+		PreviewURL:      fmt.Sprintf("https://example.com/preview/template-%s", tName),
+		PreviewImageURL: fmt.Sprintf("https://example.com/preview/template-%s.png", tName),
+	}
+
+	err := db.Create(tmpl).Error
+	Expect(err).To(BeNil())
+
+	return tmpl
+}
